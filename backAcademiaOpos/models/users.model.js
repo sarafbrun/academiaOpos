3ea9e0f8f93bcdbf653406bdadf8e431e2499ab5
sarafbrun <sarafuen.93@gmail.com>
2023-07-06 20:14:22 +0200

    Realizadas pruebas de registro y login: hay algún error, no funciona

--- backAcademiaOpos/models/users.model.js
+++ backAcademiaOpos/models/users.model.js
@@ -1,1 +1,28 @@
 //Queries fantasiosas
+
+
+const create = ({ email, password, username, role }) => {
+    return db.query('insert into users (email, password, username, role) values (?,?,?,?)', [email, password, username, role]);
+}
+
+const getByEmail = (email) => {
+    return db.query('select * from users where email = ?', [email]);
+}
+
+const getUserById = (userId) => {
+    return db.query('select * from users where id = ?', [userId]);
+}
+
+const getUserByUsername = (username) => {
+    return db.query('SELECT users.id FROM counts_app.users WHERE users.username = ?', [username])
+}
+
+const getRole = (groupId) => {
+    return db.query(`Select uhg.role FROM counts_app.users_has_groups as uhg
+JOIN counts_app.users as u ON u.id = uhg.users_id
+JOIN counts_app.groups as g ON g.id = uhg.users_id
+WHERE g.id = ? AND uhg.role = "admin"`, [groupId])
+}
+
+
+module.exports = { create, getByEmail, getUserById, getUserByUsername, getRole }
--- backAcademiaOpos/routes/opos/users.js
+++ backAcademiaOpos/routes/opos/users.js
@@ -1,9 +1,106 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const { create, getByEmail, getUserById, getRole } = require('../../models/users.model')
+const { createToken } = require('../../helpers/utils');
+const { checkToken } = require('../../helpers/middlewares')
+
+router.get('/', checkToken, async (req, res) => {
+    try {
+
+        const { username } = req.user
 
 
-router.get('/', (req, res) => {
-    res.send('HOLA');
+        res.json(username)
+    } catch (error) {
+        res.json({ fatal: error.message })
+    }
 });
+
+router.get('/user/:userId', checkToken, async (req, res) => {
+    try {
+
+        const { id } = req.user
+        const [user] = await getUserById(id)
+        res.json(user)
+    } catch (error) {
+        res.json({ fatal: error.message })
+    }
+});
+
+
+/* router.get('/get/role/:groupId', async (req, res) => {
+    try {
+
+        const { groupId } = req.params
+        const [role] = await getRole(groupId)
+        res.json(role[0].role)
+    } catch (error) {
+        res.json({ fatal: error.message })
+    }
+}); */
+
+
+
+
+
+router.get('/loggedId', checkToken, async (req, res) => {
+    try {
+
+        const { id } = req.user
+
+
+        res.json(id)
+    } catch (error) {
+        res.json({ fatal: error.message })
+    }
+});
+
+
+//POST /api/usuarios/registro
+router.post('/register', async (req, res) => {
+
+    req.body.password = bcrypt.hashSync(req.body.password, 8)
+
+    try {
+        const [result] = await create(req.body);
+        res.json(result);
+    } catch (err) {
+        res.json({ Fallo: err.message })
+    }
+});
+
+
+router.post('/login', async (req, res) => {
+    try {
+        //Existe el email en la base de datos?
+        const [result] = await getByEmail(req.body.email)
+        if (result.length === 0) {
+            return res.json({ Fatal: 'error en el email o contraseña' })
+        }
+
+        const users = result[0];
+        // ¿las passwords coinciden?
+        //tenemos la password de la base de datos y la password que le paso por el body, comprobamos si coinciden
+        const iguales = bcrypt.compareSync(req.body.password, users.password);
+        if (!iguales) {
+            return res.json({ Fatal: 'error en el email o contraseña' })
+        }
+
+
+        res.json({
+            success: 'login correcto',
+            token: createToken(users)
+        });
+
+    } catch (err) {
+        res.json({ fallo: err.message });
+    }
+})
+
+
+
+
+module.exports = router;
 
 
 module.exports = router;
